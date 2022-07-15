30cd449aebd1f87b4fc092b269c7cfd57ef4e1e9
Guilherme Mesquita <guilhermer_mesquita@hotmail.com>
2022-07-15 10:08:41 -0400

    first component

--- frontend/dsmeta/src/App.tsx
+++ frontend/dsmeta/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton/Index"
+
 function App() {
   return(
-    <h1>Olá Mundo</h1>
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton/>
+    </>
   )
 }
 
